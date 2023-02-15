3381afb048e5d62d756d81dca263f7028e77a820
Yura <0520yukendo@gmail.com>
2023-02-15 19:11:49 +0900

    Chapter3終了

---
+++ spring_security_sample/src/main/java/com/example/config/SecurityConfig.java
@@ -0,0 +1,52 @@
+package com.example.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+        .antMatchers("/loginForm").permitAll()
+        .antMatchers("/admin").hasAuthority("ADMIN") // 管理者のみ/adminにアクセスできる
+        .anyRequest().authenticated();
+
+		http.authorizeRequests()
+        .antMatchers("/loginForm").permitAll()
+        .anyRequest().authenticated();
+
+		http.formLogin()
+        .loginProcessingUrl("/login")
+        .loginPage("/loginForm")
+        .usernameParameter("email")
+        .passwordParameter("password")
+        .defaultSuccessUrl("/home", true)
+        .failureUrl("/loginForm?error");
+
+		http.logout()
+			.logoutUrl("/logout")
+			.logoutSuccessUrl("/loginForm");
+	}
+
+	public PasswordEncoder passwordencoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+	}
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/css/**", "/js/**", "/images/**");
+	}
+
+}
