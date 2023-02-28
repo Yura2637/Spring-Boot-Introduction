6f32c57ade3a81e3a646572b64d9e62fe9924649
Yuichiro <0520yukendo@gmail.com>
2023-02-28 10:47:06 +0900

    Lesson3 finish

---
+++ api_sample-1/src/main/java/com/example/ApiSample1Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSample1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSample1Application.class, args);
+	}
+
+}
